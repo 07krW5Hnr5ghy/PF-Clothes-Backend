20ad32847dd9c710e9feb0f21929e329289a7d50
Juan Romero <jryn145@gmail.com>
2023-03-16 19:22:37 -0500

    refactor(database config):commenting code to rearrange database structure

--- DataBase/db.js
+++ DataBase/db.js
@@ -8,11 +8,12 @@ const modelProfile = require("./Models/Profile");
 const modelVariant = require("./Models/Variant");
 
 //Seeders
-const users = require("./Seeders/users");
-const products = require("./Seeders/products");
-const reviews = require("./Seeders/reviews");
-const createVariant = require("./Seeders/variant");
+// const users = require("./Seeders/users");
+// const products = require("./Seeders/products");
+// const reviews = require("./Seeders/reviews");
+// const createVariant = require("./Seeders/variant");
 
+/* database conection config */
 let conn =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
@@ -44,71 +45,57 @@ let conn =
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
 
-/*
-const conn = new Sequelize("pf_cloth", DB_USER, DB_PASSWORD, {
-    host:DB_HOST,
-    dialect:"postgres",
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-})
-*/
-/*
-const conn = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pf-cloth`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-*/
 
-modelProduct(conn);
-modelCalification(conn);
-modelMarketedProduct(conn);
-modelProfile(conn);
-modelVariant(conn);
+/* connecting models */
+// modelProduct(conn);
+// modelCalification(conn);
+// modelMarketedProduct(conn);
+// modelProfile(conn);
+// modelVariant(conn);
 //console.log(conn.models)
-//Asociaciones
-const { product, qualification, marketedProduct, profile, variant } =
-  conn.models;
 
-//Perfil
-profile.hasMany(product);
-profile.hasMany(marketedProduct);
-profile.hasMany(qualification);
-//producto
-product.hasMany(qualification);
-product.hasMany(variant);
-//marketedProduct
-marketedProduct.hasOne(product);
+// Relations
+// const { product, qualification, marketedProduct, profile, variant } =
+//   conn.models;
+
+// //Perfil
+// profile.hasMany(product);
+// profile.hasMany(marketedProduct);
+// profile.hasMany(qualification);
+// //producto
+// product.hasMany(qualification);
+// product.hasMany(variant);
+// //marketedProduct
+// marketedProduct.hasOne(product);
 
 ///Seeder
+// const profilesCreator = async () => {
+//   const modelsProduct = products.map(async (pro) => {
+//     const model = await product.create(pro);
+//     await model.createVariant(createVariant());
+//     await model.createVariant(createVariant());
+//     return model;
+//   });
+//   var e = 0;
 
-const profilesCreator = async () => {
-  const modelsProduct = products.map(async (pro) => {
-    const model = await product.create(pro);
-    await model.createVariant(createVariant());
-    await model.createVariant(createVariant());
-    return model;
-  });
-  var e = 0;
+//   for (i = 0; i < users.length; i++) {
+//     const profileC = await profile.create(users[i]);
+//     console.log(profileC.__proto__)
+//     if (e <= modelsProduct.length) {
+//       await profileC.addProduct(await modelsProduct[e]);
+//       if (modelsProduct[e + 1])
+//         await profileC.addProduct(await modelsProduct[e + 1]);
+//       e += 2;
+//     }
 
-  for (i = 0; i < users.length; i++) {
-    const profileC = await profile.create(users[i]);
-    //console.log(profileC.__proto__)
-    if (e <= modelsProduct.length) {
-      await profileC.addProduct(await modelsProduct[e]);
-      if (modelsProduct[e + 1])
-        await profileC.addProduct(await modelsProduct[e + 1]);
-      e += 2;
-    }
-
-    if (reviews[i]) {
-      await profileC.createQualification(reviews[i]);
-    }
-  }
-};
+//     if (reviews[i]) {
+//       await profileC.createQualification(reviews[i]);
+//     }
+//   }
+// };
 
 module.exports = {
   conn,
-  profilesCreator,
   ...conn.models,
   Op,
 };
